d2b8d9e034eb8bb5362bc40333477831b29ec1c8
IvanovRoman <werert87@mail.ru>
2019-03-21 12:30:26 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# nodejs-example-two
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const fs = require("fs");
+
+//create a server object:
+http
+  .createServer(function(req, res) {
+    res.write("Hello World!"); //write a response to the client
+    res.end(); //end the response
+  })
+  .listen(8080); //the server object listens on port 8080
